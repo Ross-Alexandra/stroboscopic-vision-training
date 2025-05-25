857f83b3e61b52be70a7ffc0e0e51bee270d14ce
Ross Alexandra <Ross-Alexandra@outlook.com>
2025-05-24 17:37:21 -0700

    Correct the phrasing of the built-in epilepsy warning

--- __main__.py
+++ __main__.py
@@ -16,7 +16,7 @@ level_count = len(levels)
 
 
 @click.command()
-@click.option('--verify', is_flag=True, default=True, prompt='⚠️ WARNING: This program flashes rapidly and may trigger seizures in individuals with photosensitive epilepsy.\nI confirm I have read the epilepsy warning and do not have a history of photosensitive epilepsyDo not use if you have a history of seizures or epilepsy.\nBy Pressing enter, you confirm that you have read the epilepsy warning and do not have a history of photosensitive epilepsy')
+@click.option('--verify', is_flag=True, default=True, prompt='⚠️ WARNING: This program flashes rapidly and may trigger seizures in individuals with photosensitive epilepsy.\nDo not use if you have a history of seizures or epilepsy.\nBy Pressing enter, you confirm that you have read the epilepsy warning and do not have a history of photosensitive epilepsy')
 @click.option('--level', prompt=f'What level do you want to play on (1-{level_count})', type=int, default=1)
 @click.option('--custom-timings', help="Custom timings (hide_time show_time)", nargs=2, is_flag=False, type=int)
 def startStrobe(verify, level, custom_timings):
